9e287f514871552b64b82635c183df188c981725
mosleh <moslehazizi@gmail.com>
2023-09-11 18:43:30 +0330

    database was added!

--- rumiPanel/views.py
+++ rumiPanel/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomepageTemplateView(TemplateView):
+    template_name = 'home.html'
