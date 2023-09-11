6907d1eda52efd47c13058493518709a1bcf2f2b
mosleh <moslehazizi@gmail.com>
2023-09-11 21:44:19 +0330

    book model was added to rumiPanel app!

--- rumiPanel/models.py
+++ rumiPanel/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class book(models.Model):
+
+    bookId = models.CharField(_("id"), max_length=255)
+    title = models.CharField(_("title"), max_length=255)
+    subtitle = models.CharField(_("subtitle"), max_length=255, blank=True,)
+    author = models.CharField(_("author"), max_length=255)
+    publisher = models.CharField(_("publisher"), max_length=255)
+    published_date = models.DateField(_("published date"), auto_now=True)
+    category = models.CharField(_("category"), max_length=255)
+    distrubution_expense = models.FloatField(_("distribution expense"))
+
+    def __str__(self):
+        return self.title
